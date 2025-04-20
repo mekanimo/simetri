38b79fd6184ca40d99546b5dcb0962ea157d466d
Fahri Basegmez <mangabasi@gmail.com>
2025-04-20 17:07:07 -0400

    added docstrings

---
+++ src/simetri/__init__.py
@@ -0,0 +1,4 @@
+"""simetri.graphics' parent package.
+This package contains the graphics module which is the main entry point for the
+simetri library, as well as all other packages in simetri.
+"""
